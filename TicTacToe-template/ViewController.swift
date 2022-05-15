660382ecd272c87373936f4bb7b2e8698ddc0e32
BhautikPethani <c0854487@mylambton.ca>
2022-05-14 23:10:35 -0400

    Completed

--- TicTacToe-template/ViewController.swift
+++ TicTacToe-template/ViewController.swift
@@ -14,8 +14,10 @@ class ViewController: UIViewController {
     var player2 = false
     
     var board = [[0,0,0],[0,0,0],[0,0,0]]
-    var counter = 0
+    var counter = 1
     
+    @IBOutlet weak var boardImg: UIImageView!
+    @IBOutlet weak var lblResult: UILabel!
     @IBOutlet weak var One1: UIButton!
     @IBOutlet weak var One2: UIButton!
     @IBOutlet weak var One3: UIButton!
@@ -29,6 +31,11 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        if(counter % 2 == 1){
+            lblResult.text = "Player 1"
+        }else{
+            lblResult.text = "Player 2"
+        }
     }
     
     
@@ -41,7 +48,15 @@ class ViewController: UIViewController {
         player2 = false
         
         board = [[0,0,0],[0,0,0],[0,0,0]]
-        counter = 0
+        counter = 1
+        
+        if(counter % 2 == 1){
+            lblResult.text = "Player 1"
+        }else{
+            lblResult.text = "Player 2"
+        }
+        
+        boardImg.image = UIImage(named: "board")
         
         One1.isEnabled = true
         One1.setBackgroundImage(nil, for: .normal)
@@ -109,8 +124,84 @@ class ViewController: UIViewController {
         player2 = !player2
         sender.isEnabled = false
         counter+=1
+        if(counter % 2 == 1){
+            lblResult.text = "Player 1"
+        }else{
+            lblResult.text = "Player 2"
+        }
+        
+        if(checkResult()){
+            disableAllButtons()
+        }else if(counter > 9){
+            lblResult.text = "Draw"
+        }
     }
     
+    func disableAllButtons(){
+        One1.isEnabled = false
+        One2.isEnabled = false
+        One3.isEnabled = false
+        
+        Second1.isEnabled = false
+        Second2.isEnabled = false
+        Second3.isEnabled = false
+        
+        Third1.isEnabled = false
+        Third2.isEnabled = false
+        Third3.isEnabled = false
+    }
+    
+    func checkResult() -> Bool{
+        if(board[0][0] == board[0][1] && board[0][1] == board[0][2] && board[0][0] != 0){
+            //print("1");
+            lblResult.text = (board[0][0] == 1) ? "Player 1 Won" : "Player 2 Won"
+            boardImg.image = UIImage(named: "board1")
+            return true
+        }
+        else if(board[1][0] == board[1][1] && board[1][1] == board[1][2] && board[1][0] != 0){
+            //print("2");
+            lblResult.text = (board[1][0] == 1) ? "Player 1 Won" : "Player 2 Won"
+            boardImg.image = UIImage(named: "board2")
+            return true
+        }
+        else if(board[2][0] == board[2][1] && board[2][1] == board[2][2] && board[2][0] != 0){
+            //print("3");
+            lblResult.text = (board[2][0] == 1) ? "Player 1 Won" : "Player 2 Won"
+            boardImg.image = UIImage(named: "board3")
+            return true
+        }
+        else if(board[0][0] == board[1][0] && board[1][0] == board[2][0] && board[0][0] != 0){
+            //print("4");
+            lblResult.text = (board[0][0] == 1) ? "Player 1 Won" : "Player 2 Won"
+            boardImg.image = UIImage(named: "board4")
+            return true
+        }
+        else if(board[0][1] == board[1][1] && board[1][1] == board[2][1] && board[0][1] != 0){
+            //print("5");
+            lblResult.text = (board[0][1] == 1) ? "Player 1 Won" : "Player 2 Won"
+            boardImg.image = UIImage(named: "board5")
+            return true
+        }
+        else if(board[0][2] == board[1][2] && board[1][2] == board[2][2] && board[0][2] != 0){
+            //print("6");
+            lblResult.text = (board[0][2] == 1) ? "Player 1 Won" : "Player 2 Won"
+            boardImg.image = UIImage(named: "board6")
+            return true
+        }
+        else if(board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != 0){
+            //print("7");
+            lblResult.text = (board[0][0] == 1) ? "Player 1 Won" : "Player 2 Won"
+            boardImg.image = UIImage(named: "board7")
+            return true
+        }
+        else if(board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2] != 0){
+            //print("8");
+            lblResult.text = (board[0][2] == 1) ? "Player 1 Won" : "Player 2 Won"
+            boardImg.image = UIImage(named: "board8")
+            return true
+        }
+        return false
+    }
     
 }
 
